f1ea9492e866c48c61a2758c9b0b15fa80f4499a
Tony Dinh <tonydinh442@gmail.com>
2022-07-21 19:15:07 -0500

    - Initial project commit - Add images, colors, and fonts from design - Add font extension

--- Chat/Views/RootView.swift
+++ Chat/Views/RootView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  RootView.swift
 //  Chat
 //
 //  Created by Tony Dinh on 7/21/22.
@@ -7,15 +7,16 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct RootView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
+            .font(Font.pageTitle)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct RootView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        RootView()
     }
 }
