52908dfbaaed5cbcf5e597ae45548e64cfa2834f
Tony Dinh <tonydinh442@gmail.com>
2022-07-21 19:06:37 -0500

    Initial Commit

---
+++ Chat/ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApp.swift
+//  Chat
+//
+//  Created by Tony Dinh on 7/21/22.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Chat
+//
+//  Created by Tony Dinh on 7/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
